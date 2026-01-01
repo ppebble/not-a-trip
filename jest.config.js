8a67ce14ef0c5f25efdc7a05e2f7c83d63ab01bf
leesm <ask135799@naver.com>
2026-01-01 20:37:59 +0900

    fix: jest 설정을 next.js 15와 호환되도록 수정
    - next/jest 모듈 경로 수정
    - moduleNameMapper 오타 수정
    - commonjs 형식으로 변경

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-import nextJest from 'next/jest'
+const nextJest = require('next/jest.js')
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files
@@ -10,10 +10,10 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
-  moduleNameMapping: {
+  moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig)
