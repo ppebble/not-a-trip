ee8aa7e0a7f2c6b9d1bf51fb2515c86837434579
leesm <ask135799@naver.com>
2026-01-28 08:55:12 +0900

    chore: 스토어 통합 export 파일 생성

---
+++ src/stores/index.ts
@@ -0,0 +1,34 @@
+/**
+ * Zustand 스토어 통합 export
+ * 모든 전역 상태 스토어를 한 곳에서 관리합니다.
+ */
+
+// Map Store - 지도 상태 관리
+export {
+  useMapStore,
+  useMapCenter,
+  useMapZoom,
+  useSelectedSpotId,
+} from './mapStore'
+
+// UI Store - UI 상태 관리
+export {
+  useUIStore,
+  useIsPreviewOpen,
+  usePreviewSpotId,
+  useIsMobileMenuOpen,
+  useIsMapLoading,
+} from './uiStore'
+
+// Like Store - 좋아요 상태 관리
+export { useLikeStore, useLikedSceneIds, useIsLoadingLikes } from './likeStore'
+
+// Auth Store - 인증 UI 상태 관리
+export { useAuthStore, useIsLoggingOut, useAuthError } from './authStore'
+
+// Filter Store - 카테고리 필터 상태 관리
+export {
+  useFilterStore,
+  useSelectedCategories,
+  ALL_CATEGORIES,
+} from './filterStore'
