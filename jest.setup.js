9a67465eae3ac01cc2a1624661e5870d063de7b0
leesm <ask135799@naver.com>
2026-03-22 01:42:11 +0900

    chore: jest.setup.js에 ResizeObserver 글로벌 mock 추가

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,12 @@
 // Jest setup file for global test configuration
 import '@testing-library/jest-dom'
+
+// jsdom에 ResizeObserver가 없으므로 글로벌 mock 추가
+global.ResizeObserver = class ResizeObserver {
+  constructor(callback) {
+    this._callback = callback
+  }
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
