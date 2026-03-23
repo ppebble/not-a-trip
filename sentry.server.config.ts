02a7b3a1909acbf2720f2a534f5de372b117d5cc
leesm <ask135799@naver.com>
2026-03-24 02:19:06 +0900

    chore: sentry 서버/edge 초기화 설정 환경별 분리 적용

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs'
+
+import { getSentryConfig, isSentryEnabled } from '@/lib/sentry'
+
+const dsn = process.env.NEXT_PUBLIC_SENTRY_DSN
+const env = process.env.NODE_ENV ?? 'development'
+const config = getSentryConfig(env)
+
+if (isSentryEnabled(dsn)) {
+  Sentry.init({
+    dsn,
+    environment: env,
+    tracesSampleRate: config.tracesSampleRate,
+    enableLogs: true,
+  })
+}
