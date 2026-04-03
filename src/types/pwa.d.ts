4ed2b4fe7e1efa6840a2a2d1dfcf043a3de37270
leesm <ask135799@naver.com>
2026-04-03 23:49:25 +0900

    fix: beforeInstallPromptEvent 타입을 global scope로 수정

--- src/types/pwa.d.ts
+++ src/types/pwa.d.ts
@@ -7,19 +7,20 @@
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/API/BeforeInstallPromptEvent
  */
-interface BeforeInstallPromptEvent extends Event {
-  readonly platforms: string[]
-  readonly userChoice: Promise<{
-    outcome: 'accepted' | 'dismissed'
-    platform?: string
-  }>
-  prompt(): Promise<void>
-}
+
+export {}
 
 declare global {
+  interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: string[]
+    readonly userChoice: Promise<{
+      outcome: 'accepted' | 'dismissed'
+      platform?: string
+    }>
+    prompt(): Promise<void>
+  }
+
   interface WindowEventMap {
     beforeinstallprompt: BeforeInstallPromptEvent
   }
 }
-
-export {}
