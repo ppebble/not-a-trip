01d4ff1a4d8e73a4885de4b9d4f534ca57b800d9
ppebble <92935915+ppebble@users.noreply.github.com>
2026-04-15 21:54:53 +0900

    fix: directionsButton 테스트 파일 타입 참조 추가 (#487)

--- src/components/common/__tests__/DirectionsButton.test.tsx
+++ src/components/common/__tests__/DirectionsButton.test.tsx
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+/// <reference types="@testing-library/jest-dom" />
 import { render, screen, fireEvent } from '@testing-library/react'
 import DirectionsButton from '../DirectionsButton'
 
